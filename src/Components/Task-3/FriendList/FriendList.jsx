b42dd82e744492949a54c336a197edadd76a3e32
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-12-28 16:26:38 +0200

    adds propTypes to FriendList

--- src/Components/Task-3/FriendList/FriendList.jsx
+++ src/Components/Task-3/FriendList/FriendList.jsx
@@ -3,25 +3,50 @@ import css from "./FriendList.module.css";
 
 const FriendListItem = ({ name, avatar, id, isOnline }) => {
   return (
-    <li key={id} className={css.item}>
-      <span
-        className={css.status}
-        style={{ backgroundColor: !isOnline ? "red" : "green" }}
-      ></span>
-      <img
-        className={css.avatar}
-        src={avatar}
-        alt={name}
-        width="48"
-        height="48"
-      />
-      <p className={css.name}>{name}</p>
-    </li>
+    id &&
+    typeof id === "number" && (
+      <li key={id} className={css.item}>
+        <span
+          className={css.status}
+          style={{ backgroundColor: !isOnline ? "red" : "green" }}
+        ></span>
+        <img
+          className={css.avatar}
+          src={avatar}
+          alt={name}
+          width="48"
+          height="48"
+        />
+        {name && typeof name === "string" && <p className={css.name}>{name}</p>}
+      </li>
+    )
   );
 };
 
 const FriendList = ({ friends }) => (
   <ul className={css.friendList}> {friends.map(FriendListItem)} </ul>
 );
+
+FriendList.defaultProps = {
+  friends: [
+    {
+      id: 0,
+      name: "not-found",
+      avatar: "not-found",
+      isOnline: false,
+    },
+  ],
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string,
+      avatar: PropTypes.string,
+      isOnline: PropTypes.bool,
+    })
+  ),
+};
 
 export default FriendList;
